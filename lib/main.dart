296c7d230a6f7fbd7769130e7cc9f6fd56bb7ce8
MU-munn <rpgmukai333@gmail.com>
2021-10-14 15:02:11 +0900

    dairy commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,80 @@
 import 'package:flutter/material.dart';
-import 'package:karaoke_lost_app/view/home_view';
+import 'package:karaoke_lost_app/settings/theme_settings.dart';
+import 'package:karaoke_lost_app/view/karaoke_list_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'カラオケのオトモ',
       theme: ThemeData(
-        primarySwatch: Colors.orange,
+        primarySwatch: Colors.red,
       ),
-      home: MainPage(),
+      home: TabPage(),
     );
   }
 }
 //最初に表示する画面
-class MainPage extends StatelessWidget {
-
+class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title:const Text("カラオケのオトモ")),
-      body: Container(
-        child: Column(
-          children: [
-            Container(),
-            Container(),
-            Container(),
-          ],
+    Settings.deviceHeight = MediaQuery.of(context).size.height;
+    Settings.deviceWidth = MediaQuery.of(context).size.width;
+    return const Scaffold(
+      backgroundColor: Colors.orange,
+      body: Center(
+        child: Text(
+          "First Page",
+          style: TextStyle(fontSize: 20),
         ),
       ),
     );
   }
 }
 
+//タブバーを表示するWidget/////////////////////
+class TabPage extends StatefulWidget {
+  @override
+  _TabPageState createState() => _TabPageState();
+}
+
+class _TabPageState extends State<TabPage> {
+  int _selectedIndex = 0;
+
+  static List<Widget> _pageList = [
+    FirstPage(),
+    KaraokeListPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pageList[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            title: Text('Setting'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
